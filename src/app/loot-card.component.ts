d7b8330931272f49eeccb66f5a774bd6291cd99f
John Du <johndu9@gmail.com>
2024-09-17 16:19:53 -0400

    Fix tabs in loot-card

--- src/app/loot-card.component.ts
+++ src/app/loot-card.component.ts
@@ -14,81 +14,80 @@ export enum LootCardType {
 }
 
 @Component({
-    selector: 'loot-card',
-    standalone: true,
-    imports: [NgIf, NgClass, MatCardModule, MatButtonModule, MatIconModule],
-    encapsulation: ViewEncapsulation.ShadowDom, // needed for span spacing
-    templateUrl: './loot-card.component.html',
-    styleUrl: './loot-card.component.scss'
-  })
-  export class LootCardComponent {
+  selector: 'loot-card',
+  standalone: true,
+  imports: [NgIf, NgClass, MatCardModule, MatButtonModule, MatIconModule],
+  encapsulation: ViewEncapsulation.ShadowDom, // needed for span spacing
+  templateUrl: './loot-card.component.html',
+  styleUrl: './loot-card.component.scss'
+})
+export class LootCardComponent {
 
-    @Input({required: true})
-    loot!: Loot;
+  @Input({required: true})
+  loot!: Loot;
 
-    @Input({required: true})
-    cardType!: LootCardType;
+  @Input({required: true})
+  cardType!: LootCardType;
 
-    @Input()
-    isCharged: boolean = true;
+  @Input()
+  isCharged: boolean = true;
 
-    @Output()
-    useCharge = new EventEmitter<string>();
+  @Output()
+  useCharge = new EventEmitter<string>();
 
-    @Output()
-    restoreCharge = new EventEmitter<string>();
+  @Output()
+  restoreCharge = new EventEmitter<string>();
 
-    @Output()
-    select = new EventEmitter<string>();
+  @Output()
+  select = new EventEmitter<string>();
 
-    t = LootCardType;
+  t = LootCardType;
 
-    get cardTraits(): string {
-      if (this.loot.type === LootType.CONSUMABLE) {
-        return this.loot.type;
-      } else {
-        return (this.loot.type).concat(', ', this.loot.sourcePool);
-      }
-    }
-
-    get hasBasic(): boolean {
-      return this.loot.basic.length > 0;
-    }
-
-    get hasCharged(): boolean {
-      return this.loot.charged.length > 0;
-    }
-
-    get hasDescription(): boolean {
-      return this.loot.description.length > 0;
-    }
-
-    get hasFooter(): boolean {
-      return this.cardType === LootCardType.SELECT || (this.cardType === LootCardType.CHARGE && this.hasCharged);
-    }
-
-    md(input: string) {
-      const output = marked.parse(input, { async: false });
-      return DOMPurify.sanitize(output, { ALLOWED_TAGS: ['p', 'em'] });
-    }
-
-    typeToIcon(type: LootType) {
-      switch (type) {
-        case LootType.WEAPON:
-          return 'swords';
-        case LootType.CLOTHING:
-          return 'apparel';
-        case LootType.MAGIC_ITEM:
-          return 'diamond';
-        case LootType.CONSUMABLE:
-          return 'science';
-        default:
-          return 'question_mark';
-      }
-    }
-
-    typeToClass(type: LootType) {
-      return type.replace(' ', '-').toLowerCase();
+  get cardTraits(): string {
+    if (this.loot.type === LootType.CONSUMABLE) {
+      return this.loot.type;
+    } else {
+      return (this.loot.type).concat(', ', this.loot.sourcePool);
     }
   }
-  
+
+  get hasBasic(): boolean {
+    return this.loot.basic.length > 0;
+  }
+
+  get hasCharged(): boolean {
+    return this.loot.charged.length > 0;
+  }
+
+  get hasDescription(): boolean {
+    return this.loot.description.length > 0;
+  }
+
+  get hasFooter(): boolean {
+    return this.cardType === LootCardType.SELECT || (this.cardType === LootCardType.CHARGE && this.hasCharged);
+  }
+
+  md(input: string) {
+    const output = marked.parse(input, { async: false });
+    return DOMPurify.sanitize(output, { ALLOWED_TAGS: ['p', 'em'] });
+  }
+
+  typeToIcon(type: LootType) {
+    switch (type) {
+      case LootType.WEAPON:
+        return 'swords';
+      case LootType.CLOTHING:
+        return 'apparel';
+      case LootType.MAGIC_ITEM:
+        return 'diamond';
+      case LootType.CONSUMABLE:
+        return 'science';
+      default:
+        return 'question_mark';
+    }
+  }
+
+  typeToClass(type: LootType) {
+    return type.replace(' ', '-').toLowerCase();
+  }
+}
