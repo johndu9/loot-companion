7c005cf5d2f79a39efcc59388cda1d93868363a1
John Du <johndu9@gmail.com>
2024-09-24 22:46:08 -0400

    Add pool page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { PlayerComponent } from './player.component';
 import { LootListComponent } from './loot-list.component';
 import { DeleteLootComponent } from './delete-loot.component';
+import { PoolComponent } from './pool.component';
 
 export const routes: Routes = [
   { path: 'player/:id', component: PlayerComponent },
+  { path: 'pool/:id', component: PoolComponent },
   { path: 'browse', component: LootListComponent,
     data: {
       canFilter: true,
---
+++ src/app/pool.component.ts
@@ -0,0 +1,103 @@
+import { Component, Input, OnDestroy, OnInit } from "@angular/core";
+import { Loot, Player, Pool } from "./loot.defs";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { LootListComponent } from "./loot-list.component";
+import { combineLatest, Subject, takeUntil } from "rxjs";
+import { LootService } from "./loot.service";
+
+enum PoolViewMode {
+  ViewLoot,
+  AddLoot,
+  RemoveLoot,
+}
+
+@Component({
+  selector: 'pool',
+  standalone: true,
+  imports: [MatButtonModule, MatIconModule, LootListComponent],
+  templateUrl: './pool.component.html',
+  styleUrl: './pool.component.scss'
+})
+export class PoolComponent implements OnDestroy, OnInit {
+
+  private readonly unsubscribe$ = new Subject<void>();
+
+  @Input({required: true, alias: 'id', transform: (id: any) => { return +id; }})
+  poolIndex!: number;
+
+  pool?: Pool;
+  p = Player;
+
+  inPool: boolean[] = [];
+  charged: boolean[] = [];
+  canFilter: boolean = false;
+
+  loots: Loot[] = [];
+  pools: Pool[] = [];
+
+  mode: PoolViewMode = PoolViewMode.ViewLoot;
+  m = PoolViewMode;
+
+  constructor(public lootService: LootService) {
+  }
+
+  ngOnInit(): void {
+    combineLatest([this.lootService.loots$, this.lootService.pools$]).pipe(takeUntil(this.unsubscribe$)).subscribe(([loots, pools]) => {
+      const pool = pools[this.poolIndex];
+      this.pool = pool;
+      this.loots = loots;
+      this.pools = pools;
+      if (pool) {
+        this.inPool = loots.map((l, i) => pool.loots.includes(i));
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+  }
+
+  modeToHidden(mode: PoolViewMode) {
+    switch (mode) {
+      case PoolViewMode.ViewLoot:
+      case PoolViewMode.RemoveLoot:
+        return this.inPool.map(i => !i);
+      case PoolViewMode.AddLoot:
+        return this.inPool;
+    }
+  }
+
+  modeToButtonText(mode: PoolViewMode): string {
+    switch (mode) {
+      case PoolViewMode.RemoveLoot: return 'Remove';
+      case PoolViewMode.AddLoot: return 'Add';
+      default: return '';
+    }
+  }
+
+  modeToButtonIcon(mode: PoolViewMode): string {
+    const buttonText = this.modeToButtonText(mode);
+    switch (buttonText) {
+      case 'Remove': return 'remove';
+      case 'Add': return 'add';
+      default: return '';
+    }
+  }
+
+  onSelect(event: {name: string, index: number}) {
+    if (this.pool) {
+      switch (this.mode) {
+        case PoolViewMode.RemoveLoot: {
+          this.lootService.moveLootToPool(event.index, this.pools.findIndex(p => this.loots[event.index].sourcePool === p.name));
+          break;
+        }
+        case PoolViewMode.AddLoot: {
+          this.lootService.chargeLoot(event.index);
+          this.lootService.moveLootToPool(event.index, this.poolIndex);
+          break;
+        }
+      }
+    }
+  }
+}
